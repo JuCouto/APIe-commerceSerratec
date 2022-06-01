7385078d5b7dca68b79f5ec331de360275631043
CauaKnG <cauadefreitas030204@gmail.com>
2022-06-01 09:48:55 -0300

    Criando os Controllers

---
+++ ecommerce/src/main/java/com/example/ecommerce/controllers/EnderecoController.java
@@ -0,0 +1,69 @@
+package com.example.ecommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommerce.entities.Endereco;
+import com.example.ecommerce.services.EnderecoService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/endereco")
+public class EnderecoController {
+	@Autowired
+	EnderecoService enderecoService;
+
+	@GetMapping
+	public ResponseEntity<List<Endereco>> findAllEndereco() {
+		List<Endereco> enderecoList = enderecoService.findAllEndereco();
+		if (enderecoList.isEmpty()) {
+			return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(enderecoList, HttpStatus.OK);
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Endereco> findEnderecoById(@PathVariable Integer id) {
+		Endereco endereco = enderecoService.findEnderecoById(id);
+		if (endereco == null) {
+			return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(endereco, HttpStatus.OK);
+		}
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Endereco> saveEndereco(@RequestBody Endereco endereco) {
+		Endereco novoEndereco = enderecoService.saveEndereco(endereco);
+		return new ResponseEntity<>(novoEndereco, HttpStatus.CREATED);
+	}
+
+	@PutMapping
+	public ResponseEntity<Endereco> updateEndereco(@RequestBody Endereco endereco) {
+		Endereco novoEndereco = enderecoService.updateEndereco(endereco);
+		return new ResponseEntity<>(novoEndereco, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteEndereco(@PathVariable Integer id) {
+		if (enderecoService.findEnderecoById(id) == null) {
+			return new ResponseEntity<>("Esse endereco não foi encontrado", HttpStatus.NO_CONTENT);
+		} else {
+			enderecoService.deleteEndereco(id);
+			return new ResponseEntity<>("Endereco deletado com sucesso", HttpStatus.OK);
+		}
+	}
+}
---
+++ ecommerce/src/main/java/com/example/ecommerce/controllers/ProdutoController.java
@@ -0,0 +1,69 @@
+package com.example.ecommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommerce.entities.Produto;
+import com.example.ecommerce.services.ProdutoService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+	@Autowired
+	ProdutoService produtoService;
+
+	@GetMapping
+	public ResponseEntity<List<Produto>> findAllProduto() {
+		List<Produto> produtoList = produtoService.findAllProduto();
+		if (produtoList.isEmpty()) {
+			return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(produtoList, HttpStatus.OK);
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> findProdutoById(@PathVariable Integer id) {
+		Produto produto = produtoService.findProdutoById(id);
+		if (produto == null) {
+			return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(produto, HttpStatus.OK);
+		}
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> saveProduto(@RequestBody Produto produto) {
+		Produto novoProduto = produtoService.saveProduto(produto);
+		return new ResponseEntity<>(novoProduto, HttpStatus.CREATED);
+	}
+
+	@PutMapping
+	public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
+		Produto novoProduto = produtoService.updateProduto(produto);
+		return new ResponseEntity<>(novoProduto, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteProduto(@PathVariable Integer id) {
+		if (produtoService.findProdutoById(id) == null) {
+			return new ResponseEntity<>("Esse produto não foi encontrado", HttpStatus.NO_CONTENT);
+		} else {
+			produtoService.deleteProduto(id);
+			return new ResponseEntity<>("Produto deletado com sucesso", HttpStatus.OK);
+		}
+	}
+}
