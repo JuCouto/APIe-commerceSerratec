e13779ada71e637fea1e5ac11aa03634013691e3
Ju Couto <jnalmeida@ymail.com>
2022-06-01 13:12:32 -0300

    Adicionando DTO

---
+++ ecommerce/src/main/java/com/example/ecommerce/dtos/ClienteDTO.java
@@ -0,0 +1,66 @@
+package com.example.ecommerce.dtos;
+
+public class ClienteDTO {
+	
+	private Integer idCliente;
+
+	private String emailCliente;
+
+	private String nomeCliente;
+
+	private String cpfCliente;
+
+	private String telefoneCliente;
+	
+	private EnderecoDTO endereco;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getEmailCliente() {
+		return emailCliente;
+	}
+
+	public void setEmailCliente(String emailCliente) {
+		this.emailCliente = emailCliente;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getCpfCliente() {
+		return cpfCliente;
+	}
+
+	public void setCpfCliente(String cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+
+	public String getTelefoneCliente() {
+		return telefoneCliente;
+	}
+
+	public void setTelefoneCliente(String telefoneCliente) {
+		this.telefoneCliente = telefoneCliente;
+	}
+
+	public EnderecoDTO getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(EnderecoDTO endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
---
+++ ecommerce/src/main/java/com/example/ecommerce/dtos/ItemPedidoDTO.java
@@ -0,0 +1,86 @@
+package com.example.ecommerce.dtos;
+
+public class ItemPedidoDTO {
+	
+	private Integer idItemPedido;
+
+	private Integer qtdItemPedido;
+
+	private Double precoVenda;
+
+	private Double percentualDesconto;
+
+	private Double valorBruto;
+
+	private Double valorLiquido;
+
+	private PedidoDTO pedido;
+
+	private ProdutoDTO produto;
+
+	public Integer getIdItemPedido() {
+		return idItemPedido;
+	}
+
+	public void setIdItemPedido(Integer idItemPedido) {
+		this.idItemPedido = idItemPedido;
+	}
+
+	public Integer getQtdItemPedido() {
+		return qtdItemPedido;
+	}
+
+	public void setQtdItemPedido(Integer qtdItemPedido) {
+		this.qtdItemPedido = qtdItemPedido;
+	}
+
+	public Double getPrecoVenda() {
+		return precoVenda;
+	}
+
+	public void setPrecoVenda(Double precoVenda) {
+		this.precoVenda = precoVenda;
+	}
+
+	public Double getPercentualDesconto() {
+		return percentualDesconto;
+	}
+
+	public void setPercentualDesconto(Double percentualDesconto) {
+		this.percentualDesconto = percentualDesconto;
+	}
+
+	public Double getValorBruto() {
+		return valorBruto;
+	}
+
+	public void setValorBruto(Double valorBruto) {
+		this.valorBruto = valorBruto;
+	}
+
+	public Double getValorLiquido() {
+		return valorLiquido;
+	}
+
+	public void setValorLiquido(Double valorLiquido) {
+		this.valorLiquido = valorLiquido;
+	}
+
+	public PedidoDTO getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(PedidoDTO pedido) {
+		this.pedido = pedido;
+	}
+
+	public ProdutoDTO getProduto() {
+		return produto;
+	}
+
+	public void setProduto(ProdutoDTO produto) {
+		this.produto = produto;
+	}
+	
+	
+}
