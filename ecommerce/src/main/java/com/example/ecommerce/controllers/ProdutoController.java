7c7a892f39d692384fd2119cfcca15e56c817aef
Ju Couto <jnalmeida@ymail.com>
2022-06-02 20:53:59 -0300

    Adicionando save Produto Com Foto

--- ecommerce/src/main/java/com/example/ecommerce/controllers/ProdutoController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controllers/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.ecommerce.entities.Produto;
 import com.example.ecommerce.services.ProdutoService;
@@ -49,6 +52,15 @@ public class ProdutoController {
 		Produto novoProduto = produtoService.saveProduto(produto);
 		return new ResponseEntity<>(novoProduto, HttpStatus.CREATED);
 	}
+	
+	@PostMapping(value="/com-foto", consumes = {MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.MULTIPART_FORM_DATA_VALUE})
+	public ResponseEntity<Produto> saveProduto(@RequestPart("produto") String produto, 
+			@RequestPart("file") MultipartFile file) throws Exception{
+		Produto novoProduto = produtoService.saveProdutoComFoto(produto,file);
+		return new ResponseEntity<> (novoProduto, HttpStatus.CREATED);
+	}
+	
 
 	@PutMapping
 	public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
--- ecommerce/src/main/java/com/example/ecommerce/services/ProdutoService.java
+++ ecommerce/src/main/java/com/example/ecommerce/services/ProdutoService.java
@@ -1,15 +1,18 @@
 package com.example.ecommerce.services;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.ecommerce.dtos.CategoriaDTO;
 import com.example.ecommerce.dtos.ProdutoDTO;
 import com.example.ecommerce.entities.Categoria;
 import com.example.ecommerce.entities.Produto;
 import com.example.ecommerce.repositories.ProdutoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class ProdutoService {
@@ -19,6 +22,12 @@ public class ProdutoService {
 	
 	@Autowired
 	CategoriaService categoriaService;
+	
+	@Autowired
+	ArquivoService arquivoService;
+	
+	@Autowired
+	MailService mailService;
 	
 	public List<Produto> findAllProduto() {
 		return produtoRepository.findAll();
@@ -45,6 +54,30 @@ public class ProdutoService {
 		Produto produto = converterDTOParaEntidade(produtoDTO);
 		Produto novoProduto = produtoRepository.save(produto);
 		return converterEntidadeParaDTO(novoProduto);
+	}
+	
+	public Produto saveProdutoComFoto(String produtoString, MultipartFile file) throws Exception{
+		Produto produtoConvertido = new Produto();
+		try {
+			ObjectMapper objMapper = new ObjectMapper();
+			produtoConvertido = objMapper.readValue(produtoString,Produto.class);
+		} catch (IOException e) {
+			System.out.println("Ocorreu um erro ao salvar imagem");
+		}
+		Produto produtoBD = produtoRepository.save(produtoConvertido);
+		produtoBD.setImagemProduto(produtoBD.getIdProduto()+ "_" + file.getOriginalFilename());
+		Produto produtoAtualizado = produtoRepository.save(produtoBD);
+		try {
+			arquivoService.criarArquivo(produtoBD.getIdProduto() + "_" + file.getOriginalFilename(),file);
+			
+		} catch (Exception e) {
+			throw new Exception ("Não foi possível mover o arquivo.-" + e.getStackTrace());
+		} arquivoService.criarArquivo(produtoBD.getIdProduto() + "_" + file.getOriginalFilename(),file);
+		
+		String corpoEmail = "Foi cadastrado uma nova categoria " + produtoAtualizado.toString(); 
+		mailService.enviarEmailTexto("teste@teste.com", "cadastroProduto", corpoEmail);
+		
+		return produtoAtualizado;
 	}
 	
 	public Produto updateProduto(Produto produto) {
